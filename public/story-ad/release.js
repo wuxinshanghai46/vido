67c5772c207088465cacad716e4b00f6c1ec99bf
傅兴 <fu-xing46@users.noreply.gitee.com>
2026-08-24 02:38:37 +0800

    build(story-ad): build V201u immutable release

--- public/story-ad/release.js
+++ public/story-ad/release.js
@@ -1,2 +1,2 @@
-export const CLIENT_BUILD_ID = "20260824-production-v201t";
+export const CLIENT_BUILD_ID = "20260824-production-v201u";
 export const CLIENT_CONTRACT_VERSION = "story-scene-platform-v6";
