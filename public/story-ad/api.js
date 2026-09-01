2a15b59e6a47da19dc10efd6ceb8a9571a705e1c
傅兴 <fu-xing46@users.noreply.gitee.com>
2026-09-01 23:54:46 +0800

    build(story-ad): publish v379 manifest

--- public/story-ad/api.js
+++ public/story-ad/api.js
@@ -1,4 +1,4 @@
-import { CLIENT_BUILD_ID, CLIENT_CONTRACT_VERSION } from './release.js?v=20260901-production-v378';
+import { CLIENT_BUILD_ID, CLIENT_CONTRACT_VERSION } from './release.js?v=20260901-production-v379';
 
 export { CLIENT_BUILD_ID, CLIENT_CONTRACT_VERSION };
 const TOKEN_KEYS = ['vido_token', 'token'];
